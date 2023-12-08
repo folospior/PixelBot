11cc4545c000af2b3a3930fe51555beea603163c
folospior <folosp2@gmail.com>
2023-12-08 19:12:17 +0100

    Add project files.

---
+++ CSharpBot/MessageContextMenus.cs
@@ -0,0 +1,14 @@
+﻿using DSharpPlus;
+using DSharpPlus.SlashCommands;
+
+namespace PixelBot
+{
+    public class MessageContextMenus : ApplicationCommandModule
+    {
+        [ContextMenu(ApplicationCommandType.MessageContextMenu, "Translate")]
+        public async Task TranslateMenu(ContextMenuContext ctx)
+        {
+
+        }
+    }
+}
